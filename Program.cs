2d9c215f7ed4c150da2e5325b34a2824f9da56df
Jorge <jepers2021@gmail.com>
2021-03-29 17:53:26 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace A133590.Ejercicio34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 34.");
+            Console.WriteLine("Programa que imprime la fecha cada 10 segundos por 60 segundos");
+
+            DateTime anterior = DateTime.Now;
+            DateTime guarda60 = DateTime.Now;
+            Console.WriteLine(anterior);
+            while(true)
+            {
+                if((DateTime.Now - anterior).TotalSeconds > 10)
+                {
+                    anterior = DateTime.Now;
+                    Console.WriteLine(anterior);
+                }
+
+                if((DateTime.Now - guarda60).TotalSeconds > 60)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+        }
+    }
+}
